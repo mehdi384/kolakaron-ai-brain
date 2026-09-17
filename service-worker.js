8514c9bd7d5565c8a16eff5767fc8303a743d396
mehdi384 <salehi191191@gmail.com>
2026-09-17 20:01:28 +0330

    Add planner service worker

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'planner-shell-v1';
+const SHELL = ['./', './index.html', './manifest.webmanifest'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
